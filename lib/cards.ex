ce90f33d3035b9bbf2709bfb0aa1b02f76c322cd
James Hunyar <jrhunyar@gmail.com>
2019-05-23 21:02:48 -0400

    Add moduledoc for Cards

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,4 +1,8 @@
 defmodule Cards do
+  @moduledoc """
+    Provides methods for creating and handling a deck of cards
+  """
+
   def create_deck do
     values = ["Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King", "Ace"]
     suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
